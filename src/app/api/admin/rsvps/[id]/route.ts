ff52a51e11f3f8631ce1f3a833c6a255ea9a6503
Gauthier Minor <gauthier.minor@gmail.com>
2025-05-16 10:04:49 +0200

    adjustments for prod

--- src/app/api/admin/rsvps/[id]/route.ts
+++ src/app/api/admin/rsvps/[id]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
@@ -6,7 +6,7 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function DELETE(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
