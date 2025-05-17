0bd9d633f170247a9dc46a7ca80ff1c9bf23ffa8
Gauthier Minor <gauthier.minor@gmail.com>
2025-05-17 13:39:05 +0200

    hm

--- src/app/api/admin/temoins/[id]/route.ts
+++ src/app/api/admin/temoins/[id]/route.ts
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export async function DELETE(
   request: NextRequest,
   { params }: any
